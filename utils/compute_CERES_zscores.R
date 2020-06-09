319ec6f2c15b60b5021adf7fffa5a3823aa7c73e
pty0111 <typark@princeton.edu>
2020-06-09 02:14:36 -0400

    init

--- utils/compute_CERES_zscores.R
+++ utils/compute_CERES_zscores.R
@@ -10,12 +10,11 @@ library(readr)
 ### load data files
 gene_effect_cor <- read_csv(paste(rawdir,"gene_effect_corrected.csv",sep=""))
 
-colnames(gene_effect_cor)[1] <- "Broad_ID"
+colnames(gene_effect_cor)[1] <- "Sample"
 
 scores <- gene_effect_cor
-samples <- scores$Broad_ID
-scores$Broad_ID <- NULL
-
+samples <- scores$Sample
+scores$Sample <- NULL
 
 
 ### compute CERES z-scores according to Meyers et al., 2017
@@ -29,17 +28,7 @@ zscores <- sweep(zscores,1,stds2,"/")
 
 CERES_zscores <- cbind(Sample = samples, zscores, stringsAsFactors = FALSE)
 
-output.file <- file(paste(profiledir,"CERES_differential_dependencies.tsv",sep=""),"wb")
-write.table(CERES_zscores,
-            row.names = FALSE,
-            col.names = TRUE,
-            sep="\t",
-            file = output.file,
-            quote = FALSE)
-close(output.file)
-
-
-#### six_sigma differential dependency summary ####
+### six_sigma differential dependency summary ####
 
 sixsigmaL <- data.frame(apply(zscores,2,function(x){x < -6}))
 sixsigmaR <- data.frame(apply(zscores,2,function(x){x > 6}))
@@ -52,6 +41,31 @@ sixsigma_summary <- data.frame(gene=colnames(zscores),sixsigmaLcount=sixsigmaLco
 sixsigma_summary$sixsigmaTotalcount <- with(sixsigma_summary, sixsigmaLcount + sixsigmaRcount)
 df_sixsigma_summary <- sixsigma_summary[,c(1,2,3,6,4,5)]
 
+### CERES scores of six-sigma genes (store for fitting with gaussian mixture)
+sixsigma_genes <- CERES_zscores_sixsigma_summary[CERES_zscores_sixsigma_summary$sixsigmaTotalcount > 0,]$gene
+gene_effect_cor_sixsigma <- gene_effect_cor[,c('Sample',sixsigma_genes)]
+
+
+
+### write output files
+
+output.file <- file(paste(profiledir,"CERES_differential_dependencies.tsv",sep=""),"wb")
+write.table(CERES_zscores,
+            row.names = FALSE,
+            col.names = TRUE,
+            sep="\t",
+            file = output.file,
+            quote = FALSE)
+close(output.file)
+
+output.file <- file(paste(profiledir,"CERES_scores_sixsigma.tsv",sep=""),"wb")
+write.table(gene_effect_cor_sixsigma,
+            row.names = FALSE,
+            col.names = TRUE,
+            sep="\t",
+            file = output.file,
+            quote = FALSE)
+close(output.file)
 
 output.file <- file(paste(profiledir,"CERES_zscores_sixsigma_summary.tsv",sep=""),"wb")
 write.table(df_sixsigma_summary,
